e090cd57a23b35b2252f1cbb03babc1ae32010cf
yo <yo@MacBook-Pro-yo.local>
2025-11-20 15:28:49 +0300

    _

--- src/users/users.middleware.ts
+++ src/users/users.middleware.ts
@@ -6,7 +6,8 @@ import { parse, validate } from '@tma.js/init-data-node';
 export class UsersMiddleware implements NestMiddleware {
     use(req: any, res: any, next: (error?: any) => void) {
         try {
-          const token = "7804873499:AAEw6N8cYxoA1N7knlLSTmBfJOCgzehQplY" // "8283812112:AAGSvxci6xh9EreWA2zpjHJJ9ijrMTRyJzk"
+          const token = "8283812112:AAGSvxci6xh9EreWA2zpjHJJ9ijrMTRyJzk"
+          // const token = "7804873499:AAEw6N8cYxoA1N7knlLSTmBfJOCgzehQplY" // "8283812112:AAGSvxci6xh9EreWA2zpjHJJ9ijrMTRyJzk"
           const [authType, authData = ''] = (req.header('authorization') || '').split(' ');
 
           console.log("auth type", authType)
